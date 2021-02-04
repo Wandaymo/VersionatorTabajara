945b880c0a230c4ba592bd44b943d5cc699d5f85
Wandaymo Gomes <wandaymo.sousa@philips.com>
2021-02-04 17:29:32 -0300

    Release one

---
+++ src/versionatortabajara/VersionatorTabajara.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package versionatortabajara;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author wgsousa
+ */
+public class VersionatorTabajara {
+    
+    public static void main(String[] args) {
+        String param = JOptionPane.showInputDialog(null, "As versões devem estar separados por vírgula");
+        String nrOS = JOptionPane.showInputDialog(null, "Número da OS");
+        String cherry = JOptionPane.showInputDialog(null, "Cherry-pick");
+        String out = "";
+        param = param.replaceAll(" ", "");
+        String[] parts = param.split("\\,");
+
+        for (int i = 0; i < parts.length; i++) {
+            if (i != 0) {
+                out = out + " ";
+            }
+            out = out + "git checkout " + parts[i] + " && git pull origin " + parts[i] + " && git checkout -b " + parts[i] + "_" + nrOS + " && git cherry-pick " + cherry + " && git push origin " + parts[i] + "_" + nrOS;
+            if(i < parts.length - 1) {
+                out = out + " &&";
+            }
+        }
+        
+        JTextArea text = new JTextArea(out);
+        JOptionPane.showMessageDialog(null, text, "Output", 1);
+        System.out.println(out); 
+    }
+}
